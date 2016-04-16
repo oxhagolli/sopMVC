2afa0997f8eb4ac5e112341d9d3dce480190f29e
Orens Xhagolli <orens.xhagolli@hotmail.com>
2016-04-16 19:02:29 -0400

    Added 'unique' to fields.

---
+++ src/sopGenerator/Field.java
@@ -0,0 +1,35 @@
+package sopGenerator;
+
+/**
+ * File:    Field.java
+ * Package: sopGenerator
+ * Project: sopMVC
+ * <p>
+ * Class Field description.
+ *
+ * @author Orens Xhagolli
+ * @version 4/16/2016
+ */
+public class Field {
+
+    String name, type;
+    boolean unique;
+
+    public Field(String name, String type, boolean unique) {
+        this.name = name;
+        this.type = type;
+        this.unique = unique;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isUnique(){
+        return unique;
+    }
+}
