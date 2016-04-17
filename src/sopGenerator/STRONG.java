6ab4b2313085e13eb415b8a6eb0545c75c6ddf58
Patrick Pistor <yogert96@gmail.com>
2016-04-17 05:26:03 -0400

    Create STRONG.java

---
+++ src/sopGenerator/STRONG.java
@@ -0,0 +1,46 @@
+package sopGenerator;
+
+import java.util.HashMap;
+
+/**
+ * File:    STRONG.java
+ * Package: sopGenerator
+ * Project: sopMVC
+ * <p>
+ * Class STRONG description.
+ *
+ * @author Orens Xhagolli
+ * @version 4/16/2016
+ */
+public class STRONG implements TAG{
+
+    HashMap<String, String> attr;
+    String content;
+
+    public STRONG(String content) {
+        attr = new HashMap<>();
+        this.content = content;
+    }
+
+
+    public void addAttr(String attribute, String value){
+        if(attr.keySet().contains(attribute))
+            return;
+        attr.put(attribute, value);
+    }
+
+    public void addContent(String content){
+        this.content += content;
+    }
+
+    public String getHTML(){
+        String ret = "<strong ";
+        for(String str: attr.keySet()){
+            ret += str + "=\"" + attr.get(str) + "\" ";
+        }
+        ret += ">";
+        ret+=content;
+        ret+= "</strong>\n";
+        return ret;
+    }
+}
